8e80f77f10cb8ff7752bcc0c14f8e707fc2c9a00
sreyesg <santiagoreyes.g@gmail.com>
2024-10-29 20:39:19 -0700

    Complete definition of StarshipCard component

--- src/components/StarshipCard.jsx
+++ src/components/StarshipCard.jsx
@@ -1,5 +1,22 @@
-const StarshipCard = (starships) => {
-    console.log(starships, 'prop in Card')
+const StarshipCard = ({starships}) => {
+    
+    return(
+        <>
+        {
+        starships.map((starship) => {
+            return(
+                <article>
+                        <h3>{starship.name}</h3>
+                        <p>Class: {starship.class_list}</p>
+                        <p>Manufacturer: {starship.manufacturer}</p>
+                        <p>Model: {starship.model}</p>
+                </article>
+            )
+                
+        })
+        }
+        </>
+    )
 }
 
 export default StarshipCard
